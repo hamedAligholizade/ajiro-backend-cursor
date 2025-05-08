7986e9078ccf46d10141c09cf277a0ed19708311
aligholizade1995 <aligholizade1995@gmail.com>
2025-05-08 21:43:26 +0330

    seed

--- startup.sh
+++ startup.sh
@@ -8,6 +8,10 @@ sleep 5
 echo "Running database migrations..."
 node src/db/migrations/migrate.js up
 
+# Run database seed
+echo "Running database seed..."
+node backend/src/db/seeders/admin-user.seed.js
+
 # Start the application
 echo "Starting the application..."
 npm run dev 
